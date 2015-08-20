9c68d51a3440f4d90f46d3cdfe439809bce29b39
Yi-Ying He <b00502040@ntu.edu.tw>
2015-08-20 22:21:25 +0800

    fix

--- js/home.js
+++ js/home.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
     $("#menu").sticky({ topSpacing: 0 });
     $("a[id^=mainbutton]").each(setButtonCSSNormal);
     $("a[id^=mainbutton]").hover(setButtonCSSHover, setButtonCSSNormal);
-	constructPictureBoxSet($("#content"), '_data/programming_list.xml', 0);
+	constructPictureBoxSet($("#content"), 'data/programming_list.xml', 0);
 });
