0354f339410f502bef2e01fe93d39edc126ffc68
Yi-Ying He <b00502040@ntu.edu.tw>
2015-08-21 15:31:18 +0800

    fix

--- js/home.js
+++ js/home.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     constructMenuList($("#menu"), 'data/mainmenu.xml');
     constructPictureBoxSet($("#content"), 'data/programming_list.xml', 1, true);
+    $("#content").css({width: Math.min(950, $(window).width())});
     $("#menu").sticky({ topSpacing: 0 });
+});
+
+$(window).resize(function(){
+    $("#content").css({width: Math.min(950, $(window).width())});
 });
